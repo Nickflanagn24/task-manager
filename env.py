facbcfb6fb8736c32a993680e5c568b7323e1f6a
Nickflanagn24 <lpao26ad@students.codeinstitute.net>
2025-02-23 23:05:34 +0000

    feat: Initial setup of task manager project
    - Set up the project structure with `run.py` as the entry point
    - Added `taskmanager` package with necessary imports
    - Configured Flask application to run with environment variables for host, port, and debug mode
    - Ensured compatibility with Flask and Flask-SQLAlchemy versions

---
+++ env.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "any_secret_key")
+os.environ.setdefault("DEBUG", "True")
+os.environ.setdefault("DEVELOPMENT", "True")
+os.environ.setdefault("DB_URL", "postgresql://neondb_owner:npg_xwLnZ8Hjai0K@ep-green-fog-a2vyxcsr.eu-central-1.aws.neon.tech/stung_swipe_march_83341")
